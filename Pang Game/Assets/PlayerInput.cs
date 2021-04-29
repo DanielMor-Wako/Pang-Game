a8b665132764a7c8507f1672d09e4b59a38b0397
Daniel-Mor <83401330+DanielMor-Wako@users.noreply.github.com>
2021-04-29 16:25:57 +0300

    Started Player Input Detection

---
+++ Pang Game/Assets/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // init player vars
+    [SerializeField] Vector2 XYMovement;
+
+    [SerializeField] Rigidbody2D m_Rigidbody;
+
+    private bool b_canMove, b_canShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitVars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void InitVars()
+    {
+        b_canMove = true;
+        b_canShoot = true;
+    }
+}
