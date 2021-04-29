ed5ccea4edad7fde8792232bb5d4bf1929817ec3
Daniel-Mor <83401330+DanielMor-Wako@users.noreply.github.com>
2021-04-29 20:39:49 +0300

    New Ball Movement script

---
+++ Pang Game/Assets/BallMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody2D m_Rigidbody;
+    public Vector2 speed;
+
+    [Range(-1, 1)]
+    public int xDirection;
+
+    void Awake() => InitVars();
+
+
+    void InitVars()
+    {
+        if (m_Rigidbody == null)
+            m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        SetBallSpeedY();
+    }
+
+    void SetBallSpeedY()
+    {
+        float xSpeed = 2.5f;
+
+        switch (this.gameObject.tag)
+        {
+            case "BallSize_6":
+            break;
+
+            case "BallSize_5":
+                break;
+
+            case "BallSize_4":
+                break;
+
+            case "BallSize_3":
+                break;
+
+            case "BallSize_2":
+                break;
+
+            case "BallSize_1":
+                break;
+
+        }
+        float ySpeed = 2.5f;
+
+        speed = new Vector2(xSpeed, ySpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
