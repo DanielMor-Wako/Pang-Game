7359ef1246fe83bec54b0a5b7b11a44062a99f20
Daniel-Mor <83401330+DanielMor-Wako@users.noreply.github.com>
2021-04-29 22:18:23 +0300

    Add ball on pop function
    Applied to all prefabs

---
+++ Pang Game/Assets/BallPop.cs
@@ -0,0 +1,17 @@
+﻿//using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallPop : MonoBehaviour
+{
+    public enum BallPrefab { None, BallSize_1, BallSize_2, BallSize_3, BallSize_4, BallSize_5 }
+    public BallPrefab nextPrefabOnPop;
+    
+    public UnityEvent OnPopEvent;
+
+    void Awake()
+    {
+        if (OnPopEvent == null)
+            OnPopEvent = new UnityEvent();
+    }
+}
