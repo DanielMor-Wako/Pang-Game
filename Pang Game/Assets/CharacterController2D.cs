6d1a44b5718250effe24357dd237444890dd3ed0
Daniel-Mor <83401330+DanielMor-Wako@users.noreply.github.com>
2021-04-29 16:45:13 +0300

    Add Player Movement Script

---
+++ Pang Game/Assets/CharacterController2D.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    // init player vars
+    [SerializeField] Rigidbody2D m_Rigidbody;
+
+    private bool b_canMove, b_canShoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitVars();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void InitVars()
+    {
+        b_canMove = true;
+        b_canShoot = true;
+    }
+}
