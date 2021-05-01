a4229eada48677560479789b2f99ce91831d978b
Daniel-Mor <83401330+DanielMor-Wako@users.noreply.github.com>
2021-05-02 01:08:24 +0300

    Basic graphics for background

---
+++ Pang Game/Assets/Scripts/BgSpriteSwitch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class BgSpriteSwitch : MonoBehaviour
+{
+    SpriteRenderer bg;
+    public Sprite[] bgs;
+    private int currentActiveSprite = 0;
+
+    private void Awake()
+    {
+        if (bg == null)
+            bg = GetComponent<SpriteRenderer>();
+    }
+
+    public int GetSpriteIndex()
+    {
+        return currentActiveSprite;
+    }
+
+    public void SwitchToSprite(int newBg)
+    {
+        if (newBg > bgs.Length)
+            newBg = bgs.Length;
+
+        bg.sprite = bgs[newBg];
+        currentActiveSprite = newBg;
+    }
+    [ContextMenu("NextBg")]
+    public void SwitchToNextSprite()
+    {
+        currentActiveSprite++;
+        if (currentActiveSprite > bgs.Length - 1)
+            currentActiveSprite = 0;
+
+        SwitchToSprite(currentActiveSprite);
+    }
+}
