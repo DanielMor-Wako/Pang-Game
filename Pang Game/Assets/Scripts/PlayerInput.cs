984f26fcee6178d49afb444a7f1174ecbdfed5d3
Daniel-Mor <83401330+DanielMor-Wako@users.noreply.github.com>
2021-05-02 02:29:11 +0300

    Add Touch Screen Input for mobile

--- Pang Game/Assets/Scripts/PlayerInput.cs
+++ Pang Game/Assets/Scripts/PlayerInput.cs
@@ -6,8 +6,10 @@ public class PlayerInput : MonoBehaviour
     // init player vars
     [Header("Player 1")]
     [SerializeField] Vector2 XYMovement1;
+    private Vector2 XYTouchMovement1;
     [Header("Player 2")]
     [SerializeField] Vector2 XYMovement2;
+    private Vector2 XYTouchMovement2;
     private Vector2 newInput;
     
     void Awake() => InitVars();
@@ -21,23 +23,58 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xMove;
+        float yMove;
+
         // update player 1 input
-        float xMove = Input.GetAxisRaw("Horizontal");
-        float yMove = Input.GetAxisRaw("Vertical");
-        
+        // check for touch input, 
+        if (XYTouchMovement1 != Vector2.zero)
+        {
+            xMove = XYTouchMovement1.x;
+            yMove = XYTouchMovement1.y;
+        } else
+        {
+            // otherwise keyboard
+            xMove = Input.GetAxisRaw("Horizontal");
+            yMove = Input.GetAxisRaw("Vertical");
+        }
         newInput = new Vector2(xMove, yMove);
-
         XYMovement1 = newInput;
-
-        // update player 2 input
-        xMove = Input.GetAxisRaw("Horizontal2");
-        yMove = Input.GetAxisRaw("Vertical2");
         
+        // update player 2 input
+        // check for touch input, 
+        if (XYTouchMovement2 != Vector2.zero)
+        {
+            xMove = XYTouchMovement2.x;
+            yMove = XYTouchMovement2.y;
+        } else
+        {
+            xMove = Input.GetAxisRaw("Horizontal2");
+            yMove = Input.GetAxisRaw("Vertical2");
+        }
         newInput = new Vector2(xMove, yMove);
-
         XYMovement2 = newInput;
     }
 
+    // allow players to move by ui_buttons
+    public void SetPlayer1xDir(int xDir)
+    {
+        XYTouchMovement1.x = xDir;
+    }
+    public void SetPlayer1yDir(int yDir)
+    {
+        XYTouchMovement1.y = yDir;
+    }
+    public void SetPlayer2xDir(int xDir)
+    {
+        XYTouchMovement2.x = xDir;
+    }
+    public void SetPlayer2yDir(int yDir)
+    {
+        XYTouchMovement2.y = yDir;
+    }
+
+    // returns the current input value
     public float GetPlayer1XInput()
     {
         return XYMovement1.x;
